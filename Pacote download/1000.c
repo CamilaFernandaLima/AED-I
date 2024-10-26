cfa79286638ab99e72f3d1d057b83d225d1bc8cb
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2024-10-26 18:39:47 -0300

    Adicionei os arquivos

---
+++ Pacote download/1000.c
@@ -0,0 +1,9 @@
+//Camila Fernanda e Silva Lima - RA: 176.165
+//"Hello World"
+
+#include <stdio.h>
+ 
+int main() {
+    printf ("Hello World!\n");
+    return 0;
+}
---
+++ Pacote download/1002.c
@@ -0,0 +1,15 @@
+//Camila Fernanda e Silva Lima - RA: 176.165
+//Área do Círculo
+
+#include <stdio.h>
+ 
+int main () {
+    
+    double raio, area;
+    scanf("%lf", &raio);
+    area = 3.14159 * (raio * raio);
+    
+    printf("A=%.4lf\n", area);
+
+    return 0;
+}
