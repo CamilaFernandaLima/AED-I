9c83a2d53c0d244098368812d02f25902a09bb60
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2024-11-24 20:57:34 -0300

    insertionsort.c

---
+++ Estudos/ordenação de vetores/insertionsort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void insertionsort(int *v, int n){
+    int i, j, aux;
+
+    for(i=1; i<n; i++){
+        aux = v[i];
+        for (j=i; (j<0) && (aux< v[j-1]); j--){
+            v[j] = v[j-1]; //desloca todos uma posição para a frente.
+        }
+        v[j] = aux;
+    }
+}
+
+//move as cartas do baralho!!
