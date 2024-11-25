28fc8a69e5c3ee09819be53aee7e6b4c52fac3b4
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2024-11-24 21:04:29 -0300

    Create selectionsort.c

---
+++ Estudos/ordenação de vetores/selectionsort.c
@@ -0,0 +1,22 @@
+//acha o melhor valor e coloca na primeira posição, depois descarta a primeira posição até acabar o vetor.
+
+#include <stdio.h>
+
+void selectionsort(int *v, int n){
+    int i, j, menor, troca;
+
+    //procura menor elemento em relação a i
+    for(i=0; i<n-1; i++){
+        menor = i;
+        for(j = i+1; j<n; j++){
+            if(v[j]< v[menor]){
+                menor = j;
+            }
+        }
+        if (i != menor){
+            troca = v[i];
+            v[i] = v[menor];
+            v[menor] = troca;
+        }
+    }
+}
